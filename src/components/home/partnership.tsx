b8741677ca0749d8d113a296581a51d7f3b6a996
Eduardo <ferreiraeduardo88@yahoo.com.br>
2023-11-21 14:25:38 -0300

    style: partnership section (home)

--- src/components/home/partnership.tsx
+++ src/components/home/partnership.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export function Partnership() {
   return (
-    <section className="container flex flex-col justify-center items-center gap-6 my-20">
-      <h2>Nossos parceiros</h2>
+    <section id="partnership" className="container flex flex-col justify-center items-center gap-6">
+      <h3>Nossos parceiros</h3>
 
       <div className="flex flex-wrap justify-center items-center gap-x-16 gap-y-5 max-w-[344px] w-full">
         <Image src={"/img/logo/logo-hapvida.png"} alt="logo da Hapvida" height={32} width={137.33} priority />
