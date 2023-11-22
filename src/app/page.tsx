4890d23aa5b95b1b3aca133ffe11539a31d3db43
Erick <erick.kuwahara79@gmail.com>
2023-11-21 21:40:27 -0300

    style: layout (home)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 import { Nav } from "@/components/home/nav";
 import { Banner } from "@/components/home/banner";
 import { Partnership } from "@/components/home/partnership";
+import { About } from "@/components/home/about";
+import { Footer } from "@/components/home/footer";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen bg-[#CBD3DE]">
-      <header className="">
+    <main className="flex flex-col min-h-screen bg-white">
+      <header>
         <Nav />
         <Banner />
       </header>
 
       <Partnership />
+
+      <About />
+
+      <Footer />
     </main>
   );
 }
